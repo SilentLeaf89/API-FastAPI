9b249dffc425c1391a86c257733ddb227feb4d53
SilentLeaf89 <hottabych1989@gmail.com>
2023-06-02 08:47:23 +0300

    fix dump mapping and settings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,49 +4,49 @@
 elasticdump \
   --input=../data/settings_movies.json \
   --output=http://elastic:9200/movies \
-  --type=settings
+  --type=settings --csvHandleNestedData
 
 elasticdump \
   --input=../data/settings_person.json \
   --output=http://elastic:9200/person \
-  --type=settings
+  --type=settings --csvHandleNestedData
 
 elasticdump \
   --input=../data/settings_genres.json \
   --output=http://elastic:9200/genres \
-  --type=settings
+  --type=settings --csvHandleNestedData
 
 # mappings
 elasticdump \
   --input=../data/mapping_movies.json \
   --output=http://elastic:9200/movies \
-  --type=mapping
+  --type=mapping --csvHandleNestedData
 
 elasticdump \
   --input=../data/mapping_person.json \
   --output=http://elastic:9200/person \
-  --type=mapping
+  --type=mapping --csvHandleNestedData
 
 elasticdump \
   --input=../data/mapping_genres.json \
   --output=http://elastic:9200/genres \
-  --type=mapping
+  --type=mapping --csvHandleNestedData
 
 # indeces
 elasticdump \
   --input=../data/index_movies.json \
   --output=http://elastic:9200/movies \
-  --type=data
+  --type=data --csvHandleNestedData
 
 elasticdump \
   --input=../data/index_person.json \
   --output=http://elastic:9200/person \
-  --type=data
+  --type=data --csvHandleNestedData
 
 elasticdump \
   --input=../data/index_genres.json \
   --output=http://elastic:9200/genres \
-  --type=data
+  --type=data --csvHandleNestedData
 
 # start app
 gunicorn main:app --workers 1 --worker-class \
